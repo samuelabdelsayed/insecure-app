d7ae4314ac8062f17695fd60a61bf69940322ab1
Samuel Abdelsayed <5210685+samuelabdelsayed@users.noreply.github.com>
2021-04-22 14:49:58 +1000

    Create hello-world.java

---
+++ src/hello-world.java
@@ -0,0 +1,10 @@
+class HelloWorld
+{
+  string username = "silly";
+  string password = "insecure_password_123";
+  
+  public static void main(String[] args) {
+    System.out.println("Hello, World!"); 
+    System.out.println("Here is my " + username + "password! " + password);
+  }
+}
