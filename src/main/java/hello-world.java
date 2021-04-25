f4075d26d171b1f555cc65945fb38970aff258d2
Samuel Abdelsayed <5210685+samuelabdelsayed@users.noreply.github.com>
2021-04-25 13:11:46 +1000

    Update hello-world.java

--- src/main/java/hello-world.java
+++ src/main/java/hello-world.java
@@ -1,13 +1,17 @@
 package hello;
 
-class HelloWorld
-{  
+public class HelloWorld
+{
+  static void insecure(String user, String pass) 
+  {
+    System.out.println("Here is my " + user + "password! " + pass);
+  } 
   public static void main(String[] args) 
   {
     String username = "silly";
     String password = "insecure_password_123";
     
     System.out.println("Hello, World!"); 
-    System.out.println("Here is my " + username + "password! " + password);
+    insecure(username, password);
   }
 }
